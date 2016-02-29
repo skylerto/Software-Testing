c059ab4aa1a3bfc9305f062a71cbcf9b01f1518c
Siraj Rauff <sirajrauff@gmail.com>
2016-02-29 09:03:31 -0500

    Adding test cases for calculateRepeatNumber

--- src/RepeatTest.java
+++ src/RepeatTest.java
@@ -1,12 +1,107 @@
 package net.sf.borg.model;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import net.sf.borg.model.Repeat;
+import net.sf.borg.model.entity.Appointment;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RepeatTest {
+
+	/*
+	 * Testing Boundry Values of Calculate Repeat Number
+	 */
+	@Test
+	public void testCalculateRepeatNumberBVT() {
+
+		/*
+		 * Boundary Values for Calendar:
+		 * 1. The same day
+		 * 2. The next day
+		 * 3. The previous day
+		 * 4. Integer.MAX_VALUE - 1 days in the future
+		 * 5. Integer.MAX_VALUE days in the future
+		 * 6. Integer.MAX_VALUE + 1 days in the future
+		 * 7. Integer.MIN_VALUE + 1 days in the past
+		 * 8. Integer.MIN_VALUE days in the past
+		 * 9. Integer.MIN_VALUE - 1 days in the past
+		 */
+
+		Calendar prevDay = new GregorianCalendar(0,0,1,0,0);
+		Calendar sameDay = new GregorianCalendar(0,0,2,0,0);
+		Calendar nextDay = new GregorianCalendar(0,0,3,0,0);
+		Appointment sampleAppt = new Appointment();
+		sampleAppt.setFrequency("DAILY");
+
+		// 1. The same day
+		sampleAppt.setDate(sameDay.getTime());
+		assertEquals(1, Repeat.calculateRepeatNumber(sameDay, sampleAppt));
+
+		// 2. The next day
+		assertEquals(2, Repeat.calculateRepeatNumber(nextDay, sampleAppt));
+
+		// 3. The previous day
+		assertEquals(0, Repeat.calculateRepeatNumber(prevDay, sampleAppt));
+
+		// 4. Integer.MAX_VALUE - 1 days in the future
+		Calendar overMaxDays = sameDay;
+		overMaxDays.add(Calendar.DAY_OF_MONTH, Integer.MAX_VALUE - 1);
+		assertEquals(Integer.MAX_VALUE, Repeat.calculateRepeatNumber(overMaxDays, sampleAppt));
+
+		// 5. Integer.MAX_VALUE days in the future
+		overMaxDays.add(Calendar.DAY_OF_MONTH, 1);
+		assertEquals(greaterThan(Integer.MAX_VALUE), Repeat.calculateRepeatNumber(overMaxDays, sampleAppt));
+
+		// 6. Integer.MAX_VALUE days in the future
+		overMaxDays.add(Calendar.DAY_OF_MONTH, 1);
+		assertEquals(greaterThan(Integer.MAX_VALUE), Repeat.calculateRepeatNumber(overMaxDays, sampleAppt));
+
+		// 7. Integer.MIN_VALUE + 1 days in the past
+		Calendar underMaxDays = sameDay;
+		underMaxDays.add(Calendar.DAY_OF_MONTH, Integer.MIN_VALUE + 1);
+		assertEquals(0, Repeat.calculateRepeatNumber(underMaxDays, sampleAppt));
+
+		// 8. Integer.MIN_VALUE days in the past
+		underMaxDays.add(Calendar.DAY_OF_MONTH, -1);
+		assertEquals(0, Repeat.calculateRepeatNumber(underMaxDays, sampleAppt));
+
+		// 9. Integer.MIN_VALUE - 1 days in the past
+		underMaxDays.add(Calendar.DAY_OF_MONTH, -1);
+		assertEquals(0, Repeat.calculateRepeatNumber(underMaxDays, sampleAppt));
+
+	}
+
+	/*
+	 * Special Value cases of Calculate Repeat Number
+	 */
+	@Test
+	public void testCalculateRepeatNumberSpecialValue() {
+
+		/*
+		 * Special cases for Appointment
+		 * 1. No Start Date
+		 * 2. No Repetition Frequency
+		 */
+		Calendar testCal = new GregorianCalendar(0,0,2,0,0);
+		Appointment testAppt = new Appointment();
+
+		// 1. No Start Date
+		assertEquals(0, Repeat.calculateRepeatNumber(testCal, testAppt));
+
+		// 2. No Repetition Frequency
+		testAppt.setDate(testCal.getTime());
+		testCal.add(Calendar.DAY_OF_MONTH, 1);
+		assertEquals(0, Repeat.calculateRepeatNumber(testCal, testAppt));
+	}
 
 	/**
 	 * Test Case to represent the Boundary Value Tests.
@@ -95,12 +190,12 @@ public class RepeatTest {
 		/*
 		 * 2. Strong Normal Test Cases :: Same as Weak Normal due to input range.
 		 */
-		
+
 		/*
 		 * 3. Weak Robust Test Cases
-		 * 
+		 *
 		 * A. Empty String, Empty String.
-		 * B. Non Constant, Non Numeric. 
+		 * B. Non Constant, Non Numeric.
 		 * C. NDAYS, Numeric String. (COVERED IN 1 and 2)
 		 * D. NDAYS, Empty String.
 		 * E. NWEEKS, Non Numeric.
@@ -108,42 +203,42 @@ public class RepeatTest {
 		 * G. NMONTHS, Empty String.
 		 * H. NYEARS, Non Numeric String.
 		 */
-		
+
 		 // A. Empty String, Empty String.
 			number = "5";
 			constant = Repeat.NYEARS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		 // B. Non Constant, Non Numeric.
 			number = "notanumber";
 			constant = "notconstant";
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		 // D. NDAYS, Empty String.
 			number = "";
 			constant = Repeat.NDAYS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		 // E. NWEEKS, Non Numeric.
 			number = "notanumber";
 			constant = Repeat.NWEEKS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
-			
+
+
 		 // G. NMONTHS, Empty String.
 			number = "";
 			constant = Repeat.NMONTHS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		 // H. NYEARS, Non Numeric String.
 			number = "notanumber";
 			constant = Repeat.NYEARS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
-		
+
+
 		/*
 		 * 4. Strong Robust Test Cases
-		 * 
+		 *
 		 * A. Constant Empty String, Empty String. (COVERED IN 3[A])
 		 * B. Constant Empty String, Non Numeric.
 		 * C. Constant Empty String, Numeric.
@@ -163,48 +258,48 @@ public class RepeatTest {
 		 * Q. NYEARS, Non Numeric String. (COVERED IN 3[H])
 		 * R. NYEARS, Numeric String (COVERED IN 1[D])
 		 */
-			
-			 
+
+
 		// B. Constant Empty String, Non Numeric.
 			number = "notanumber";
 			constant = "";
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// C. Constant Empty String, Numeric.
 			number = "5";
 			constant = "";
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// D. Non Constant, Number Empty String.
 			number = "";
 			constant = "notaconstant";
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// F. Non Constant, Numeric.
 			number = "5";
 			constant = "notaconstant";
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// H. NDAYS, Non Numeric String.
 			number = "notanumber";
 			constant = Repeat.NDAYS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// J. NWEEKS, Empty Number String.
 			number = "";
 			constant = Repeat.NWEEKS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// N. NMONTHS, Non Numeric String.
 			number = "notanumber";
 			constant = Repeat.NMONTHS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 		// P. NYEARS, Empty Number String.
 			number = "";
 			constant = Repeat.NYEARS;
 			assertEquals(Repeat.getNValue(constant + "," + number), 0);
-			
+
 	}
 
 	/**
@@ -220,33 +315,33 @@ public class RepeatTest {
 		 * C3. Input contains a valid number but not a valid constant.
 		 * C4. Input contains a constant but no number of occurrences.
 		 * C5. Input contains a valid constant and valid number.
-		 * 
+		 *
 		 * Actions:
 		 * A1. Return 0
 		 * A2. The extracted number
-		 * 
+		 *
 		 * C1 -> A1.
 		 * C2 -> A1.
 		 * C3 -> A1.
 		 * C4 -> A1.
 		 * C5 -> A2.
 		 */
-		
+
 		 // C1 -> A1.
 			assertEquals(Repeat.getNValue(""), 0);
-			
+
 		 // C2 -> A1.
 			assertEquals(Repeat.getNValue("notavalidconstant" + "," + "notavalidnumber"), 0);
-			
+
 		 // C3 -> A1.
 			assertEquals(Repeat.getNValue("notavalidconstant" + "," + "5"), 0);
-			
+
 		 // C4 -> A1.
 			assertEquals(Repeat.getNValue(Repeat.NDAYS + "," + "notavalidnumber"), 0);
 			assertEquals(Repeat.getNValue(Repeat.NDAYS + "," + "-1"), 0);
 			assertEquals(Repeat.getNValue(Repeat.NDAYS + "," + "2.5"), 0);
 			assertEquals(Repeat.getNValue(Repeat.NDAYS + "," + "0"), 0);
-			
+
 		 // C5 -> A2.
 			assertEquals(Repeat.getNValue(Repeat.NDAYS + "," + "5"), 5);
 			assertEquals(Repeat.getNValue(Repeat.NWEEKS + "," + "5"), 5);
